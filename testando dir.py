38fd97d4db2d44724e91a182399873714aa1ada0
Thalles Bhering <tmbhering@gmail.com>
2025-01-25 16:53:11 -0300

    Rev01 - I.A - Retorno de atividades em jupyter

---
+++ testando dir.py
@@ -0,0 +1,30 @@
+with open("C:\\Users\\Administrador\\Desktop\\filtro espiral em jupyter\\arquivos-gerados\\arquivos-txt\\variacoes-erros-montagem.txt", "r", encoding="utf-8") as f:
+    for i, line in enumerate(f):
+        print(f"Linha {i}: '{line}'")
+
+        Resultado retornado após rodar o código a cima:
+       
+        PS C:\Users\Administrador\Desktop\filtro espiral em jupyter> & C:/Users/Administrador/AppData/Local/Programs/Python/Python313/python.exe "c:/Users/Administrador/Desktop/filtro espiral em jupyter/testando dir.py"
+Linha 0: '| STATUS DA EXECUÇÃO CORRETO        |
+'
+Linha 1: '|-----------------------------------|
+'
+Linha 2: '| MONTAGEM                          |
+'
+Linha 3: '
+'
+Linha 4: '| STATUS DA EXECUÇÃO INCORRETOS     |
+'
+Linha 5: '|-----------------------------------|
+'
+Linha 6: '| Montagem andaime                  |
+'
+Linha 7: '| montagn di andaim                 |
+'
+Linha 8: '| Montagen de andaime               |
+'
+Linha 9: '| motagen                           |
+'
+Linha 10: '| Muntagin                          |
+'
+Linha 11: '| montagem de andami                |
